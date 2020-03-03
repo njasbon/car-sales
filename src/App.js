34c412e1597f2672b038083e5184f753340a15bb
Nicolle Jasbon <njasbon@gmail.com>
2020-03-02 22:30:53 -0500

    adding buttons and car images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import CarImage from "./CarImage";
+import Row from 'react-bootstrap/Row'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App () {
+
+  const [carColor, setCarColor] = useState("Red");
+
+
+return (
+    <div>
+      <CarImage color={carColor}/>
+      {carColor}
+      <Row>
+        <Button variant="danger" onClick={() => setCarColor("Red")}>Red</Button>
+        <Button variant="primary" onClick={() => setCarColor("Blue")}>Blue </Button>
+        <Button variant="warning" onClick={() => setCarColor("Yellow")}>Yellow</Button>
+      </Row>
     </div>
-  );
-}
 
-export default App;
+    ) 
+}
